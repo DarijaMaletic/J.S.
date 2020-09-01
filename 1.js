23debaf8ddff7d46a6359f76958155e08b778a50
DarijaMaletic <70064757+DarijaMaletic@users.noreply.github.com>
2020-09-01 21:32:32 +0200

    zadatak

--- 1.js
+++ 1.js
@@ -1,16 +1,13 @@
-let x= 5 
-while (x>0) {
-    console.log('Hello world')
-    //x = x-1
-    x--
-    // x -= 1
-}
+console.log ('Hello world!')
+var x=3 // = operator dodele 
+let y=54
+const z=23232 // ne mozemo da menjamo
 
-let y=1
-let brojac=0
-while(y<Infinity) {
-    console.log(y)
-    y *=2 
-    brojac++
-}
-console.log('petlja se izvrsila:',brojac,'puta')
+// let a = 3*x+y 
+let stranica = 3
+let povrsina = stranica * stranica
+let a = 234324 
+let P= a** 2 // a*a
+let O=4*a
+
+console.log (P,O)
